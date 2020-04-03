9ce0f6851106c8dfac9ad4fa3b222fccbe630398
austinb847 <austinb847@icloud.com>
2020-04-03 15:37:52 -0700

    check if userinput num is a number. if its not alert the user to enter one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,35 @@
 //back end logic
 var toRoboger = function(number) {
-  if (number > 0) {
-    var robogers = "";
-    for(i = 0; i <= number; i++) {
-      var numStr = i.toString();
-      //console.log(numStr);
-      var countOf1 = 0;
-      var countOf2 = 0;
-      var countOf3 = 0;
-      for(var e = 0; e <= numStr.length; e++) {
-        if (numStr[e] === "1") {
-          countOf1 = countOf1 + 1;
-        }
-        if (numStr[e] === "2") {
-          countOf2 = countOf2 + 1;
-        }
-        if (numStr[e] === "3") {
-          countOf3 = countOf3 + 1;
-        }
+  var robogers = "";
+  for(i = 0; i <= number; i++) {
+    var numStr = i.toString();
+    //console.log(numStr);
+    var countOf1 = 0;
+    var countOf2 = 0;
+    var countOf3 = 0;
+    for(var e = 0; e <= numStr.length; e++) {
+      if (numStr[e] === "1") {
+        countOf1 = countOf1 + 1;
       }
-
-      if (countOf3 > 0) {
-        robogers = "Won't you be my neighbor?";
-      } else if (countOf2 > 0) {
-        robogers = "Boop!";
-      } else if (countOf1 > 0) {
-        robogers = "Beep!";
-      } else {
-        robogers = numStr;
+      if (numStr[e] === "2") {
+        countOf2 = countOf2 + 1;
       }
-      $(".outputRobogers").append("<li>" + robogers + "</li>");
+      if (numStr[e] === "3") {
+        countOf3 = countOf3 + 1;
+      }  
     }
 
-  } else {
-    return false;
+    if (countOf3 > 0) {
+      robogers = "Won't you be my neighbor?";
+    } else if (countOf2 > 0) {
+      robogers = "Boop!";
+    } else if (countOf1 > 0) {
+      robogers = "Beep!";
+    } else {
+      robogers = numStr;
+    }
+    $(".outputRobogers").append("<li>" + robogers + "</li>");
   }
-
-
 };
 
 
@@ -45,7 +38,12 @@ $(document).ready(function() {
   $("form#roboger").submit(function(event) {
     event.preventDefault();
     var userNum = parseInt($("#userNum").val());
-    toRoboger(userNum);
+
+    if (isNaN(userNum) || userNum === "") {
+      alert("Enter a number please!")
+    } else {
+      toRoboger(userNum);
+    }
 
     $(".numInput").text(userNum);
     $("#result").show();
